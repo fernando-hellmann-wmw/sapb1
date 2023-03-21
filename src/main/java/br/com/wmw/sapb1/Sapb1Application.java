3da3c1af753bff85790b028be4f89669e3a374f5
Fernando <fernando.hellmann@wmw.com.br>
2023-03-21 16:40:35 -0300

    Commit de criação do projeto

---
+++ src/main/java/br/com/wmw/sapb1/Sapb1Application.java
@@ -0,0 +1,13 @@
+package br.com.wmw.sapb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sapb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sapb1Application.class, args);
+	}
+
+}
